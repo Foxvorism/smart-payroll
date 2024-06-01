bf0bed246400e4f8f05ce7f57d921dfd777677d8
“Foxvore” <imjohani2005@gmail.com>
2024-06-01 21:01:06 +0700

    feat: Create user seeder using faker

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Carbon\Carbon;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Factory::create('id_ID');
+
+        $createdAt = Carbon::today()->toDateString();
+        $updatedAt = Carbon::today()->toDateString();
+        $roles = Role::pluck('id')->toArray();
+
+        for ($i = 0; $i < 5; $i++) {
+            User::insert([
+                'id_role' => $faker->randomElement($roles),
+                'name' => $faker->name,
+                'username' => 'maul' . $i + 1,
+                'password' => Hash::make('rahasia'),
+                'created_at' => $createdAt,
+                'updated_at' => $updatedAt,
+            ]);
+        }
+    }
+}
